6f715bed81883499cb74a5a59dc994bbcf8cdbef
Gil Burns <gil@gilburns.com>
2025-08-11 17:02:08 -0500

    Initial Commit

---
+++ IntuneLogWatch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IntuneLogWatch
+//
+//  Created by Gil Burns on 8/11/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
