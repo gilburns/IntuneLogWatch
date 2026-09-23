5c584078d0afeb5a2a87c3f98e28987160d8c274
Gil Burns <gil@gilburns.com>
2026-09-22 20:31:38 -0500

    Rev version numbers for a new release

--- CLI/main.swift
+++ CLI/main.swift
@@ -5,7 +5,7 @@ struct IntuneLogWatchCLI: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "intunelogwatch-cli",
         abstract: "Inspect MDM certificates from the command line",
-        version: "2.0.0"
+        version: "2.0.1"
     )
     
     @Flag(name: .shortAndLong, help: "Output in JSON format")
